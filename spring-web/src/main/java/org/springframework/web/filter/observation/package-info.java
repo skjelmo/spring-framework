9d4fe3dcf3a5f84dfac97362d19244be185b81f8
Brian Clozel <bclozel@vmware.com>
2022-08-26 17:23:17 +0200

    Add HttpRequestsObservationFilter
    This `Filter` can be used to instrument Servlet-based web frameworks for
    Micrometer Observations.

---
+++ spring-web/src/main/java/org/springframework/web/filter/observation/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Filter implementation for observing Servlet-based web applications.
+ */
+@NonNullApi
+@NonNullFields
+package org.springframework.web.filter.observation;
+
+import org.springframework.lang.NonNullApi;
+import org.springframework.lang.NonNullFields;
